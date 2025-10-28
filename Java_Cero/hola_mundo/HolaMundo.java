7f6017ef50969c97ff3c808be188dee8ab979b3b
lfguaman <ivoylucho1990@gmail.com>
2025-10-27 19:05:11 -0500

    Refactor HolaMundo class to adjust variable initialization and output logic

--- Java_Cero/hola_mundo/HolaMundo.java
+++ Java_Cero/hola_mundo/HolaMundo.java
@@ -3,10 +3,15 @@ package Java_Cero.hola_mundo;
 public class HolaMundo {
     public static void main(String[] args) {
         String saludar = "Hola Mundo";
-        System.out.println(saludar.toUpperCase() + " Mi nombre es Luis");
+       // System.out.println(saludar.toUpperCase() + " Mi nombre es Luis");
 
-        int numero1 = 10;
-        System.out.println("El valor de numero1 es: " + numero1);
+        int numero1 = 2;
+        if (numero1>5)
+        {
+            String nombre2 = "Luis Fernando";
+            System.out.println("El nombre es "+ nombre2);
+        }else{
+        System.out.println("El valor "+numero1+ " es < que 5  ");
     }
-
+    }
 }
